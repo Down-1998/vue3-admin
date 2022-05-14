ea746f3561f90bf23fb106eb543d4948c631a4d8
wanxinlun <1255225664@qq.com>
2022-05-15 01:07:31 +0800

    feat:完善tabs的删除和批量操作

--- vue3-admain/src/layout/components/TabBar/index.vue
+++ vue3-admain/src/layout/components/TabBar/index.vue
@@ -4,24 +4,32 @@
     type="card"
     @tab-click="ClickTabs"
     @tab-remove="removeTab"
+    @contextmenu.prevent.native="openContextMenu($event)"
   >
     <el-tab-pane
       v-for="item in tabsList"
       :key="item.path"
       :label="item.title"
       :name="item.path"
-      closable="true"
+      :closable='true'
     >
       {{ item.content }}
     </el-tab-pane>
   </el-tabs>
+  <ul v-show="contextMenuVisible" class="operationTabs" :style="{left:left + 'px',top:top + 'px'}">
+      <li @click="closeAll">关闭所有</li>
+      <li @click="closeOther('left')">关闭左边</li>
+      <li @click="closeOther('right')">关闭右边</li>
+      <li @click="closeOther('other')">关闭其他</li>
+  </ul>
 </template>
 
 <script lang="ts" setup>
 import { useStore } from 'vuex'
-import { onMounted ,computed, watch, ref } from "vue";
-import { Itype} from '@/store/type.ts'
+import { onMounted ,computed, watch, ref,Ref } from "vue";
+import { Itype} from '../../../store/type'
 import { useRoute, useRouter } from 'vue-router'
+import { ElMessage } from 'element-plus';
 
 const store = useStore();
 const route = useRoute();//路由信息
@@ -32,6 +40,8 @@ const tabsList = computed(() => {
     return store.getters.getAddTabs
 })
 
+
+
 const activeKey = ref('');
 
 //添加tab
@@ -39,7 +49,7 @@ const addTab = () =>{
     const { meta, path } = route;
     const tab:Itype = {
         path,
-        title:meta.title
+        title:(meta.title) as string
     }
     store.commit('addTab',tab)
 
@@ -57,7 +67,7 @@ const ClickTabs = (event:any) => {
 }
 
 //移除tab
-const removeTab = (event:string) => {
+const removeTab = (event:any) => {
     if(tabsList.value.length === 1) {
          ElMessage({
             message: '选项卡至少保留一个',
@@ -87,7 +97,7 @@ const refresh = () => {
     let session = sessionStorage.getItem('TABS_ROUTES')
     if (session) {
         let tabItem = JSON.parse(session)
-        tabItem.forEach((tab: Itab) => {
+        tabItem.forEach((tab: Itype) => {
             store.commit('addTab', tab)
         })
     }
@@ -100,9 +110,71 @@ onMounted(() => {
     //刷新保存数据
     refresh();
 })
+//右键点击出来批量操作
+const contextMenuVisible:Ref<boolean> = ref(false)
+const left = ref('');
+const top = ref('')
+//批量操作tabs'
+const openContextMenu = (event:any) =>{
+   if(event.srcElement.id){
+        let currentTabId = event.srcElement.id.split('-')[1];//拿到路径
+        store.commit('saveCurrentTabId',currentTabId);
+        contextMenuVisible.value = true
+        left.value = event.clientX;
+        top.value = event.clientY + 10
+   }
+    
+}
+
+//关闭所有
+const closeAll = () => {
+    store.commit('closeAlltabs');
+     contextMenuVisible.value = false;
+     router.push('/index');
+}
+//关闭其他
+const closeOther = (params:string) =>{
+    store.commit('closeOtherTabs',params);
+     contextMenuVisible.value = false;
+}
+//监控删除列表,如有点击就会消失
+watch(() => contextMenuVisible.value,()=>{
+    if(contextMenuVisible.value){
+        window.addEventListener('click',()=>{
+            contextMenuVisible.value = false;
+        })
+    }else{
+        window.removeEventListener('click',()=>{
+            contextMenuVisible.value = false;
+        })
+    }
+})
 
 </script>
 
 <style lang="scss" scoped>
-
+.operationTabs{
+    position: absolute;
+    width: 100px;
+    border: 1px solid #ccc;
+    background-color: #fff;
+    z-index: 100;
+    list-style: none;
+    padding: 5px 0;
+    border-radius: 4px;
+    font-size: 14px;
+    color: #333;
+    box-shadow: 2px 2px 3px rgba(0,0,0,0.2);
+    li{
+        padding: 7px 16px;
+        cursor: pointer;
+        
+        &:hover{
+            background: #f2f2f2;
+        }
+        &:nth-of-type(1),&:nth-of-type(2),&:nth-of-type(3) {
+            border-bottom: 1px solid #f2f2f2;
+        }
+    }
+}
 </style>
--- vue3-admain/src/store/index.ts
+++ vue3-admain/src/store/index.ts
@@ -3,12 +3,14 @@ import { Itype} from './type'
 
 interface Store{
     tabsList:Array<Itype>
+    contextMenuTabId:string
 }
 
 export const store = createStore<Store>({
     state(){
          return {
-             tabsList:[]
+             tabsList:[],
+             contextMenuTabId:''
          }
     },
     mutations:{
@@ -25,6 +27,29 @@ export const store = createStore<Store>({
                 return item.path === tabName
             })
             state.tabsList.splice(index,1);
+        },
+        //右键保存tab菜单点击的tab(路径path)
+        saveCurrentTabId(store:Store,currMenuTabId:string){
+            store.contextMenuTabId = currMenuTabId;
+        },
+        //删除所有tabs
+        closeAlltabs(store:Store){
+            store.tabsList = [];
+            sessionStorage.removeItem('TABS_ROUTES');
+        },
+        //关闭其他Tabs(包含left right other)
+        closeOtherTabs(store:Store,action:string){
+            if(action === 'other'){
+                store.tabsList = store.tabsList.filter((item:Itype) =>{
+                    return store.contextMenuTabId === item.path
+                })
+            }else if(action === 'left'){
+                const index = store.tabsList.findIndex((item:Itype) => store.contextMenuTabId === item.path)
+                store.tabsList.splice(0, index);
+            }else if(action === 'right'){
+                const index = store.tabsList.findIndex((item:Itype) => store.contextMenuTabId === item.path)
+                store.tabsList.splice(index + 1);
+            }
         }
     },
     getters:{
