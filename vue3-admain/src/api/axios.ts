b9ce4bab5ce81f2aaf4dff4d9a83324d99dd7f8e
wanxinlun <1255225664@qq.com>
2022-05-16 00:05:04 +0800

    制作登录页

--- vue3-admain/src/api/axios.ts
+++ vue3-admain/src/api/axios.ts
@@ -10,13 +10,13 @@ axiosInstance.interceptors.response.use(response => {
     if(res.code === 200){
         return res;
     }else if(res.code === 10001){
-        ElMessage.error('登陆状态异常,请重新登陆')
+        // ElMessage.error('登陆状态异常,请重新登陆')
         return res;
     }else if(res.code === 10002){
-        ElMessage.error('权限不足');
+        // ElMessage.error('权限不足');
         return res;
     }else{
-        ElMessage.error(res.message);
+        // ElMessage.error(res.message);
         return res;
     }
 })
