3ded03e6cca980cdbd69ffc86323ad8f4ee48612
wanxinlun <1255225664@qq.com>
2022-05-21 22:43:15 +0800

    feat:添加角色授权页面

--- vue3-admain/src/views/system/Account.vue
+++ vue3-admain/src/views/system/Account.vue
@@ -2,6 +2,35 @@
 <template>
   <!-- <el-button @click="handleClick">按钮</el-button> -->
   <!-- 用户表格 -->
+  <div>
+    <el-button type='primary' class="add-button" @click="toAddUser">新增系统用户</el-button>
+  </div>
+   <!-- 新增用户form表单 -->
+      <el-dialog :title="state.formTitle" v-model="state.userFormDialogVisible">
+          <el-form
+          ref="userForm"
+          :model="state.userFormData"
+          :rules="state.rules"
+          label-width="100px"
+          >
+              <el-form-item label="用户名" prop="username">
+                  <el-input v-model="state.userFormData.username" placeholder="请输入用户名"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button @click="resetForm"> 重置</el-button>
+                <el-button type="primary" @click="handelConfirm"> 确定</el-button>
+              </el-form-item>
+
+          </el-form>
+      </el-dialog>
+      <!-- 角色表单
+       -->
+       <el-dialog title="设置角色" v-model="state.showSetRoleDialog">
+            <el-select @change="upRole" value="选择角色" placeholder="选择角色">
+                <el-option v-for="item in roles" :key="item.id" :label="item.name" :value="item.id"></el-option>
+            </el-select>
+       </el-dialog>
+       <!-- 系统用户表单 -->
   <div class="table-containier">
     <el-table
       :border="true" 
@@ -15,17 +44,17 @@
               v-model="scope.row.status"
               active-color="green"
               inactive-color ="red"
+              @change="(value:boolean | number | string)=> commitStatusChange(value,scope.row)"
               >
-
               </el-switch>
           </template>
         </el-table-column>
       <el-table-column label="操作" width="300px">
               <template #default="scope">
-                  <el-button type='success' size="small" >授权</el-button>
-                  <el-button type='primary' size="small" >重置密码</el-button>
-                  <el-button  type='info' size="small" >编辑</el-button>
-                  <el-button type='danger' size="small">删除</el-button>
+                  <el-button type='success' size="small" @click="toSetRole(scope.row.id)">授权</el-button>
+                  <el-button type='primary' size="small" @click="resetPw(scope.row.id)">重置密码</el-button>
+                  <el-button  type='info' size="small"  @click="toEditUser(scope.row)">编辑</el-button>
+                  <el-button type='danger' size="small" @click="deleteUser(scope.row.id)">删除</el-button>
               </template>
            </el-table-column>
     </el-table>
@@ -45,7 +74,7 @@
 </template>
 
 <script setup lang='ts'>
-import { ComponentInternalInstance, getCurrentInstance, onMounted, reactive } from 'vue'
+import { ComponentInternalInstance, getCurrentInstance, onMounted, reactive,Ref, ref } from 'vue'
 import {
   addSysUser,
   deleteSysUsers,
@@ -62,9 +91,32 @@ const { proxy } = getCurrentInstance() as ComponentInternalInstance
 const state = reactive({
   users:[],
   currentPage:1,
-  pageSize:2,//每页显示10条
+  pageSize:10,//每页显示10条
   userFormDialogVisible:false,
+  userFormData:{
+   username:''
+ },
+ rules:{
+   username:[
+     {
+       required:true,
+       message:'请输入用户名',
+       trigger:'blur'
+     }
+   ]
+ },
+ formTitle:'',
+ showSetRoleDialog:false,
+ currentUserId:0
 })
+
+interface Role {
+  id:number,
+  name:string
+}
+
+const roles:Ref<Array<Role>| null>  = ref(null)
+
 //初始化
 onMounted(() =>{
   getUsers()
@@ -85,6 +137,93 @@ const handelCurrentChange = (val:number) =>{
 const handelSizeChange = (val:number) =>{
   state.pageSize = val
 }
+//新增系统用户
+const toAddUser = () =>{
+  state.formTitle ='添加用户'
+  state.userFormDialogVisible = true
+}
+//重置
+const resetForm = () =>{
+  state.userFormData = {
+    username:''
+  }
+}
+//确定添加或者修改
+const handelConfirm = () =>{
+  if(state.formTitle.startsWith('添加用户')) {
+    addSysUser(state.userFormData).then(res=>{
+      console.log(res);
+      if(res.data == 1) {
+        proxy?.$Alert('添加成功')
+        getUsers()
+        state.userFormDialogVisible = false
+      }
+      
+  })
+  } else if(state.formTitle.startsWith('修改用户')) {
+    updateSysUser(state.userFormData).then(result =>{
+       proxy?.$Alert('更新成功')
+        getUsers()
+       state.userFormDialogVisible = false
+    })
+  }
+}
+//删除
+const deleteUser = (id:number) =>{
+  proxy?.$Confirm("确认要删除当前用户吗?").then(()=>{
+      deleteSysUsers(id).then(()=>{
+        proxy?.$Notify.success({
+          title:'删除成功',
+          duration:500
+        })
+        getUsers()
+      })
+    })
+}
+//编辑
+const toEditUser = (selecteUser:object) =>{
+  state.userFormData = JSON.parse(JSON.stringify(selecteUser))
+  state.formTitle = '修改用户'
+  state.userFormDialogVisible = true
+}
+interface User {
+  id:number,
+  status:boolean
+}
+//改变状态
+const commitStatusChange = (value:boolean | number | string,user:User) =>{
+  proxy?.$Confirm(value===false ? '确定冻结用户?' : '确定激活用户?').then(()=>{
+      updateStatus(user.id,user.status).then(()=>{
+        proxy?.$Notify.success(value===false ? '已冻结' : '已激活')
+      })
+    })
+}
+//重置密码
+const resetPw = (userId:number) =>{
+  proxy?.$Confirm('重置该用户密码,请谨慎操作!').then(()=>{
+    resetPassword(userId).then(()=>{
+      proxy?.$Notify.success('密码已经重置成功,默认重置密码:123456')
+    })
+  })
+}
+//角色授权
+const toSetRole = (userId:number) =>{
+  getAllRoles().then(result=>{
+    roles.value = result.data
+    state.currentUserId = userId
+    state.showSetRoleDialog = true
+  })
+}
+// 确认设置角色
+const upRole = (roleId:number) =>{
+  setRole(state.currentUserId,roleId).then(result=>{
+     if(result.data === 1) {
+       proxy?.$Notify.success('角色设置成功')
+       getUsers()
+       state.showSetRoleDialog = false
+     }
+  })
+}
 </script>
 <style lang='scss' scoped>
 .table-containier{
@@ -94,5 +233,8 @@ const handelSizeChange = (val:number) =>{
 .pagination{
   float: right;
   margin-top: 20px;
+}
+.add-button{
+  margin: 20px 0;
 }
 </style>
--- vue3-admain/src/views/system/Group.vue
+++ vue3-admain/src/views/system/Group.vue
@@ -1,9 +1,214 @@
 <!--  -->
 <template>
-  <div>Group</div>
+  <div>
+    <!-- 添加角色 -->
+    <div class="role-button">
+       <el-button type="primary" plain @click="toAdd">添加角色</el-button>
+    </div>
+    <!-- 添加和修改弹窗 -->
+    <el-dialog :title="state.formTitle" v-model="state.dialogVisible">
+      <el-form ref="elForm" :model="state.formData" :rules="state.rules" label-width="100px">
+        <el-form-item label="角色名称" prop="name">
+          <el-input
+            v-model="state.formData.name"
+            placeholder="请输入角色名称"
+            :maxlength="20"
+            :style="{ width: '100%' }"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="描述">
+          <el-input
+            v-model="state.formData.description"
+            placeholder="请输入角色描述"
+            :maxlength="40"
+            :style="{ width: '100%' }"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-button  @click="resetForm" >重置</el-button>
+            <el-button type="primary" @click="handelConfirm">确定</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+    <!-- 角色授权弹窗 -->
+    <el-dialog 
+      title="角色授权" 
+      v-model="state.showSetPermissionDialog"
+      destroy-on-close
+    >
+        <el-form>
+          <el-form-item>
+            <el-tree
+              :data="state.permissionTree"
+              show-checkbox
+              :props="state.defaultProps"
+              node-key="permission"
+              :default-checked-keys="state.permissions"
+              ref="permissionRef"
+            >
+            </el-tree>
+          </el-form-item>
+          <el-form-item>
+            <el-button @click="resetChecked">清空</el-button>
+            <el-button type="primary" @click="setPermission">确认</el-button>
+          </el-form-item>
+      </el-form>
+    </el-dialog>
+    <!-- 角色table表单 -->
+    <div class="group-container">
+      <el-table :data="state.roles" :border="true" >
+        <el-table-column prop="name" label="角色名称"></el-table-column>
+        <el-table-column prop="description" label="描述"></el-table-column>
+        <el-table-column label="操作">
+          <template #default="scope">
+            <el-button type="success" size="small" @click="toSetpermissions(scope.row)">授权</el-button>
+            <el-button type="info"  size="small" @click="edit(scope.row)">编辑</el-button>
+            <el-button type="danger"  size="small" @click="delRoles(scope.row.id)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
 </template>
 
 <script setup lang='ts'>
+import { ComponentInternalInstance, getCurrentInstance, onMounted, reactive, ref} from 'vue';
+import permissionTree from '@/router/permissionTree'
+import type { ElTree } from 'element-plus'
+import { deleteRoles, getAllRoles, updateRole, updateRolePermission, addRole, getPermissionsOfRole } from '@/api/system/role'
+const { proxy } = getCurrentInstance() as ComponentInternalInstance
+
+const state = reactive({
+  roles: [],
+  formTitle: '',
+  dialogVisible: false,//添加或者删除弹框
+  formData: {
+    id: 0,
+    name: '',
+    description: ''
+  },
+  rules: {
+    name: [{
+      required: true,
+      message: '请输入角色名称',
+      trigger: 'blur'
+    }],
+    description: [],
+  },
+   showSetPermissionDialog:false,//角色授权弹框
+   permissionTree:permissionTree,
+   defaultProps:{
+     id:'title',
+     label:'title',
+     children:'children'
+   },
+   permissions:[]
+})
+const permissionRef = ref<InstanceType<typeof ElTree>>()
+
+//初始化
+onMounted(() => {
+  _getAllRoles()
+})
+
+// 获取所有角色数据
+const _getAllRoles = () => {
+  getAllRoles().then(result => {
+    state.roles = result.data
+
+  })
+}
+//添加按钮
+const toAdd = () =>{
+  state.dialogVisible = true
+  state.formTitle = '添加角色'
+  resetForm()
+}
+
+//角色授权
+const toSetpermissions = (selecteRole:any) =>{
+  state.permissions=[]
+  state.formData = JSON.parse(JSON.stringify(selecteRole))
+    state.showSetPermissionDialog = true
+    getPermissionsOfRole(selecteRole.id).then(result=>{
+      console.log(result)
+      state.permissions = result.data
+    })
+}
+//角色编辑
+const edit = (selecteRole:object) =>{
+  state.formData = JSON.parse(JSON.stringify(selecteRole))
+  state.dialogVisible = true;
+  state.formTitle = '更新角色'
+}
+//角色删除
+const delRoles = (id:number) =>{
+  proxy?.$Confirm('确认要删除角色吗?').then(()=>{
+      deleteRoles([id]).then(()=>{
+        proxy?.$Notify.success('删除成功')
+        _getAllRoles()
+      })
+    }).catch(()=>{
+     
+    })
+}
+//添加和修改弹框重置
+const resetForm = () =>{
+  state.formData = {
+    id: 0,
+    name: '',
+    description: ''
+  }
+}
+//添加和修改弹框确定
+const handelConfirm = () =>{
+  if(state.formTitle === '添加角色') {
+    addRole(state.formData).then(()=>{
+        proxy?.$Notify.success('添加角色成功')
+        _getAllRoles()
+        state.dialogVisible = false
+    })
+  } else if(state.formTitle === '更新角色') {
+    updateRole(state.formData).then(()=>{
+      proxy?.$Notify.success('更新角色成功')
+      _getAllRoles()
+        state.dialogVisible = false
+    })
+  }
+}
+
+//角色授权树形控件清空操作
+const resetChecked = () =>{
+  permissionRef.value!.setCheckedKeys([],false)
+}
+//角色授权树形控件确认按钮
+const setPermission = () =>{
+  let nodes = permissionRef.value!.getCheckedNodes(false, false)
+  console.log(nodes)
+  let permissions:InstanceType<typeof ElTree>[] = []
+  nodes.forEach(node =>{
+    if(node.permission) {
+       permissions.push(node.permission)
+    }
+  })
+  let vo = {
+    roleId:state.formData.id,
+    permissions:permissions
+  }
+  updateRolePermission(vo).then(()=>{
+    proxy?.$Notify.success("修改成功")
+    state.showSetPermissionDialog = false
+  })
+}
+
 </script>
 <style lang='scss' scoped>
+.group-container{
+  margin: 5px 10px; 
+  text-align: left;
+}
+.role-button{
+  margin-left: 20px;
+  margin-bottom: 20px;
+}
 </style>
