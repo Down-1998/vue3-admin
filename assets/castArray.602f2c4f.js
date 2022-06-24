4ec9c2cf7ec3a57251d0ca751c8e452aeeb46894
Down-1998 <67084458+Down-1998@users.noreply.github.com>
2022-06-25 00:08:23 +0800

    Add files via upload

---
+++ assets/castArray.602f2c4f.js
@@ -0,0 +1,1 @@
+import{z as a}from"./index.e442f220.js";function e(){if(!arguments.length)return[];var r=arguments[0];return a(r)?r:[r]}export{e as c};
---
+++ assets/index2.e54a1baa.js
@@ -0,0 +1,1 @@
+import{bl as u,bm as s}from"./index.e442f220.js";var m={beforeMount(n,t){let e=null,a;const l=()=>t.value&&t.value(),o=()=>{Date.now()-a<100&&l(),clearInterval(e),e=null};u(n,"mousedown",r=>{r.button===0&&(a=Date.now(),s(document,"mouseup",o),clearInterval(e),e=setInterval(l,100))})}};export{m as R};
